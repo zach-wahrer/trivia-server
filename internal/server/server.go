adec3b54455adcf21ccbc4c8304c7e1a88d0643e
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-19 17:21:09 -0600

    Modify server.go for placeholder index handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,13 +7,16 @@ import (
 )
 
 func StartServer() {
-	http.HandleFunc("/", EndPointHandler)
+	http.HandleFunc("/", IndexHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `Welcome to the Trivia Server`)
 }
 
 func EndPointHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-
 	io.WriteString(w, `{"alive": true}`)
 }
