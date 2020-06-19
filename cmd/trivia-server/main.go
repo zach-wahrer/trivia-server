b15cd11c230d1e5f74899e621e88db52b5e4444c
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-19 17:14:45 -0600

    Fix directory typo

---
+++ cmd/trivia-server/main.go
@@ -0,0 +1,12 @@
+// Triva-server fetches questions from the opentdb.com API and displays them
+// on request.
+
+package main
+
+import (
+	"internal/server"
+)
+
+func main() {
+	server.StartServer()
+}
