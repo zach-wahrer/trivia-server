92770b869c921d510eb2c9dd3bfb8442601bc444
Zach Wahrer <zachtheclimber@gmail.com>
2020-06-19 18:57:06 -0600

    Add in-progress for trivia package

--- internal/trivia/trivia.go
+++ internal/trivia/trivia.go
@@ -1,13 +1,24 @@
 // Package trivia interfaces with the opentdb.com API to fetch trivia
 package trivia
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 const APIURL = "https://opentdb.com/api.php?amount=1"
 
 type Trivia struct {
 	ResponseCode int
-	Results      map[string]string
+	Test         string
+	// Results
+}
+
+type Results struct {
+	Category      string
+	Difficulty    string
+	Question      string
+	CorrectAnswer string
 }
 
 func GetTrivia() (*Trivia, error) {
@@ -21,7 +32,11 @@ func GetTrivia() (*Trivia, error) {
 	return new(Trivia), nil
 }
 
-func ProcessJSON(data []byte) (*Trivia, error) {
+func ProcessJSON(data string) (*Trivia, error) {
+	var result Trivia
+	if err := json.Unmarshal([]byte(data), &result); err != nil {
+		return nil, err
+	}
 
-	return new(Trivia), nil
+	return &result, nil
 }
--- internal/trivia/trivia_test.go
+++ internal/trivia/trivia_test.go
@@ -14,23 +14,24 @@ func TestAPICall(t *testing.T) {
 		t.Errorf("triviaAPI returned unexpected result: got code %d", trivia.ResponseCode)
 	}
 
-	if len(trivia.Results) == 0 {
-		t.Errorf("triviaAPI returned empty result")
-	}
+	// if trivia.Question == "" {
+	// 	t.Errorf("triviaAPI returned empty result")
+	// }
 
 }
 
 func TestProcessJSON(t *testing.T) {
-	testReply := []byte(`{"response_code":0,"results":[{"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"What is the name of the default theme that is installed with Windows XP?","correct_answer":"Luna","incorrect_answers":["Neptune","Whistler","Bliss"]}]}`)
+	// data := {"response_code":0,"results":[{"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"What is the name of the default theme that is installed with Windows XP?","correct_answer":"Luna","incorrect_answers":["Neptune","Whistler","Bliss"]}]}`
+	data := `{"response_code":5,"test":"this is the test string"}`
 
-	trivia, err := ProcessJSON(testReply)
+	trivia, err := ProcessJSON(data)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if trivia.Results["category"] != "Science" {
-		t.Errorf("processJSON did not correctly convert JSON: got \"%s\" want \"Science\"",
-			trivia.Results["category"])
-	}
+	t.Errorf("%v", trivia)
+	// if trivia.Category != "Science" {
+	// 	t.Errorf("processJSON did not correctly convert JSON: got \"%s\" want \"Science\"",
+	// 		trivia.Category)
+	// }
 
 }
